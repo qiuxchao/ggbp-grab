8ac797575fc48fe37cc55d6a62a8bb7f8a4375b1
qiuxchao <2915241235@qq.com>
2022-11-30 19:37:22 +0800

    update: 完成批量下载功能

--- background.js
+++ background.js
@@ -1,29 +1,36 @@
+// 发送批量下载成功消息
+const sendBatchSuccessMsg = async () => {
+  const { tabId } = await chrome.storage.local.get(['tabId']);
+  chrome.tabs.sendMessage(
+    tabId,
+    {
+      type: "one-detail-img-downloaded",
+    }
+  );
+}
 
 // 监听事件
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log(sender.tab ? "来自内容脚本: " : "来自扩展程序: ", request, sender?.tab?.url ?? '');
   const { type, payload } = request;
-  // 内容脚本的下载事件
   if (type === "download") {
-    // 请求下载权限
-    chrome.permissions.request({
-      permissions: ['downloads']
-    }, (granted) => {
-      if (granted) {
-        // 授权成功
-        payload.forEach(item => {
-          const [extName] = item.url.match(/\.(png|jpg|gif|jpeg|webp)$/)
-          chrome.downloads.download({
-            url: item.url,
-            filename: `呱呱爆品素材/${item.title}/${item.index}${extName}`,
-            conflictAction: 'overwrite',  // 覆盖相同文件
-          }, (downloadId) => {
-            console.log('[ downloadId ] >', downloadId)
-          })
-        })
-      }
-    });
+    // 内容脚本的下载事件
+    const { urlList, isBatch } = payload;
+    urlList.forEach(item => {
+      const [extName] = item.url.match(/\.(png|jpg|gif|jpeg|webp)$/)
+      const filename = `呱呱爆品素材/${item.title}/${item.index}${extName}`;
+      chrome.downloads.download({
+        url: item.url,
+        filename,
+        conflictAction: 'overwrite',  // 覆盖相同文件
+      }, (downloadId) => {
+        downloadId && console.log('[ 下载成功 ] >', filename)
+      })
+    })
+    isBatch && sendBatchSuccessMsg();
+
   } else if (type === 'devtools-send-data') {
+    // devtools 捕获到的商品列表数据
     const response = payload ? JSON.parse(payload) : null;
     console.log('[ devtools 请求信息 ] >', response)
     if (response?.data?.itemList) {
@@ -51,6 +58,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
           }
         })
     }
+
+  } else if (type === 'save-tab-id') {
+    // 批量下载图片，记录下首页 tabId
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      await chrome.storage.local.set({ tabId: tabs[0].id, });
+      sendResponse(true)
+    })
+    // return true 以使用异步的 sendResponse
+    return true;
   }
-}
-);
+});
--- content/index.js
+++ content/index.js
@@ -1,42 +1,89 @@
+// 商品元素列表
+let goodsElList = [];
+
 // 监听事件
-chrome.runtime.onMessage.addListener((request) => {
-    console.log('[ request ] >', request)
-    const { type } = request;
-    if (type === 'send-data-success') {
-      // 数据发送成功
-      alert('发送成功！')
+chrome.runtime.onMessage.addListener(async (request) => {
+  console.log('[ request ] >', request)
+  const { type } = request;
+  if (type === 'send-data-success') {
+    // 数据发送成功
+    alert('发送成功！')
+  } else if (type === 'one-detail-img-downloaded') {
+    // 一个详情的图片下载完毕
+    // 获取当前的下标
+    let { detailIndex } = await chrome.storage.local.get(['detailIndex'])
+    detailIndex++;
+    if (detailIndex + 1 > goodsElList.length) {
+      alert('图片全部下载完成！');
+      // 删除缓存下标
+      chrome.storage.local.remove(['detailIndex', 'tabId'])
+    } else {
+      handleOpenDetail(detailIndex);
     }
   }
+}
 )
 
+// 批量下载图片
+function handleBatchDownloadImg() {
+  goodsElList = $('.five-box');
+  if (goodsElList.length) {
+    // 通知后台脚本记录当前tabId，内容脚本无权限
+    chrome.runtime.sendMessage({
+      type: "save-tab-id"
+    }, function (success) {
+      success && handleOpenDetail(0);
+    });
+  } else {
+    alert('没有获取到商品列表！');
+  }
+}
+
+// 打开详情页面
+async function handleOpenDetail(index) {
+  if (!goodsElList[index]) return;
+  // 存储当前下标
+  await chrome.storage.local.set({ detailIndex: index });
+  goodsElList[index].click();
+
+}
+
 // 下载图片
-function handleDownload(res) {
+function handleDownload(isBatch = false) {
   console.log('下载图片');
   const title = $('#__next > section > main > div > div.my-15.flex.rounded-8.bg-white.p-24 > div.flex-1 > div.text-20.line-clamp-1')?.text() || '无标题';
   const urlList = [];
   const imgList = $('#__next > section > main > div > div.mt-15.overflow-hidden.rounded-8.bg-white > div.p-24 > div.mb-30 > img').each(function (index) {
     const url = $(this).attr('src')
-    // 内容脚本无法请求权限，需要把数据发送给 service_worker 处理
     urlList.push({
       url,
       title,
       index: index + 1
     });
   })
-  urlList.length && chrome.runtime.sendMessage({
-    type: "download", payload: urlList
+  chrome.runtime.sendMessage({
+    type: "download", payload: { urlList, isBatch }
   });
+  isBatch && setTimeout(window.close, 1000);
 }
 
 // 添加下载图片按钮
-const addDownloadBtn = () => {
-  $('body').append(`
+async function addDownloadBtn() {
+  const { detailIndex } = await chrome.storage.local.get(['detailIndex'])
+  if (detailIndex === undefined) {
+    $('body').append(`
       <div class="gg-helper-btn-wrap">
         <button id="gg-download-btn" class="gg-helper-btn">下载图片</button>
       </div>
     `)
+    $('#gg-download-btn').click(() => handleDownload())
+  } else {
+    // 批量下载进来的
+    setTimeout(() => {
+      handleDownload(true);
+    }, 1000)
+  }
 
-  $('#gg-download-btn').click(handleDownload)
 }
 
 // 发送请求
@@ -51,15 +98,17 @@ function handleFetchData() {
   });
 }
 
-// 添加发送数据按钮
-const addSendBtn = () => {
+// 添加发送数据&批量下载图片按钮
+function addSendBtn() {
   $('body').append(`
       <div class="gg-helper-btn-wrap">
-        <button id="gg-send-btn" class="gg-helper-btn">发送数据</button>
+        <button id="gg-send-btn" class="gg-helper-btn">发送数据</button><br/>
+        <button id="gg-batch-btn" class="gg-helper-btn">批量下载图片</button>
       </div>
     `)
 
   $('#gg-send-btn').click(handleFetchData)
+  $('#gg-batch-btn').click(handleBatchDownloadImg)
 }
 
 $(document).ready(function () {
@@ -76,12 +125,15 @@ $(document).ready(function () {
   // 监听storage变化
   chrome.storage.onChanged.addListener((changes, area) => {
     console.log('storage变化：', changes);
-    const btnEl = $(isDetail ? '#gg-download-btn' : '#gg-send-btn');
-    if (changes.on.newValue && !btnEl.length) {
-      isDetail ? addDownloadBtn() : addSendBtn();
-    } else {
-      changes.on.newValue ? btnEl.fadeIn() : btnEl.fadeOut();
+    if (changes.hasOwnProperty('on')) {
+      const btnEl = $(isDetail ? '#gg-download-btn' : '#gg-send-btn');
+      if (changes.on.newValue && !btnEl.length) {
+        isDetail ? addDownloadBtn() : addSendBtn();
+      } else {
+        changes.on.newValue ? btnEl.fadeIn() : btnEl.fadeOut();
+      }
     }
+
   });
 
 });
